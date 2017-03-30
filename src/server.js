cb28356b5ec191d160f36beab3a1832be8770a06
Zach Legein <zlegein@gmail.com>
2017-03-30 16:26:00 -0500

    save latest

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ let connector = new _connector.default();
 
 app.get('/run/:commit', async function(req, res) {
   try {
-    res.send(await connector.run(req.query.appId));
+    res.send(await connector.run(req.query.appId, req.params.commit));
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -34,9 +34,9 @@ app.get('/status/:testId', async function(req, res) {
     }
 });
 
-app.get('/results/:testId', async function(req, res) {
+app.get('/results/:testId/:commit', async function(req, res) {
     try {
-        res.send(await connector.results(req.params.testId));
+        res.send(await connector.results(req.params.testId, req.params.commit));
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
@@ -45,16 +45,16 @@ app.get('/results/:testId', async function(req, res) {
 
 app.get('/results/:commit', async function(req, res) {
     try {
-        res.send(await connector.results(req.query.id));
+        res.send(await connector.results(req.query.id, req.params.commit));
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
 });
 
-app.get('/tag/:testId', async function(req, res) {
+app.get('/tag/:testId/:commit', async function(req, res) {
     try {
-        res.send(await connector.tag(req.params.testId));
+        res.send(await connector.tag(req.params.testId, req.params.commit));
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
